30c61ee598517a9486bfedc5eb8c3530d51b46b1
Fabiana <fabianarsantos@gmail.com>
2022-04-20 15:32:12 -0300

    Doctests

---
+++ testes_doc.py
@@ -0,0 +1,17 @@
+def soma(x, y):
+    """Soma x e y
+    >>> soma(10, 20)
+    31
+    """
+    assert isinstance(x, (int, float)), 'x precisa ser int ou float.'
+    assert isinstance(y, (int, float)), 'y precisa ser int ou float.'
+    return x + y 
+
+if __name__== '__main__':
+    import doctest
+    doctest.testmod()
+    doctest.testmod(verbose=True)
+
+
+# Não colocar o nome do arquivo igual ao nome do teste!!!!!
+# Dá conflito e o Python entra em crise de identidade :)))))
